aa409987b6aa99d2b25befe02256e40e76c64b94
Ellieisjsieje <hsyny8865@gmail.com>
2026-06-27 12:17:49 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN   = os.environ.get("BOT_TOKEN", "اینجا_توکن_خودت_رو_بذار")
+BOT_TOKEN   = os.environ.get("BOT_TOKEN", "8616189984:AAFvz0gJ8Zmozv-yQWjH5pf2vu9LwxD7T7Y")
 ADMIN_IDS   = [7219535751]   # آیدی اصلی ادمین
